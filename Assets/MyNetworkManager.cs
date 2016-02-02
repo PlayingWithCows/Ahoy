188834ff81965e2d17475211f15cc6f7ba2bc488
PlayingWithCows <wiealbern@gmail.com>
2016-02-02 15:32:28 +0100

    Made MyNetWorkManager and start host work

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void MyStartHost(){
+		
+	StartHost();
+
+	}
+
+	public override void OnStartHost(){
+		Debug.Log ("Server Started at: " + System.DateTime.Now);
+	}
+}
